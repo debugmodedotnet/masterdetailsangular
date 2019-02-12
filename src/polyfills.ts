17f1b872994ff18d9ed2fa37d7f2d9f090c241a3
debugmodedotnet <debugmode@outlook.com>
2019-02-12 13:08:05 +0530

    second cmmit

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -79,6 +79,8 @@
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
+import 'core-js/es7/reflect';
+
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
